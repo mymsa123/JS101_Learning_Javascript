4c5493a8af8e6f9c750c48a36080c7f8561596a1
mymsa123 <sabir70779560@gmail.com>
2023-01-03 00:03:20 +0530

    Commit from location one

---
+++ day1 Programm/problem2.js
@@ -0,0 +1,6 @@
+let Name = "Md Sabir Alam"
+console.log(Name)
+Name = "Md Akhter Alam"
+console.log(Name)
+Name = "Angita Asif"
+console.log(Name)
---
+++ day1 Programm/problem3.js
@@ -0,0 +1,4 @@
+let name = "Md Sabir Alam"
+let age = 26
+console.log(name,age)
+console.log(typeof(name),typeof(age))
